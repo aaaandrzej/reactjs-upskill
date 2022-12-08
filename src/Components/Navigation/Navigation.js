9f49287193e0c732359adac161a9b79cab04f9b8
Andrzej Szulc <andrzej.szulc@gmail.com>
2022-12-08 16:43:29 +0100

    Fix nav bar bug

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 
-export const Navigation = (routes) => (
+export const Navigation = ({routes}) => (
   <nav>
-    {routes.map(({ route, label }) => (
+    {Object.values(routes).map(({ route, label }) => (
       <LinkButton to={route} label={label} key={route} />
     ))}
   </nav>
