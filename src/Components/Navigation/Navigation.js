a0818861b1ec5cf91fb8cb6a4b495371f567b8e7
Andrzej Szulc <andrzej.szulc@gmail.com>
2022-12-12 15:50:56 +0100

    Use AppBar for navigation

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,19 +1,151 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import MenuItem from "@mui/material/MenuItem";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 
-export const Navigation = ({routes}) => (
-  <nav>
-    {Object.values(routes).map(({ route, label }) => (
-      <LinkButton to={route} label={label} key={route} />
-    ))}
-  </nav>
-);
+// TODO extract RouteItems to a function like below so it can be used with different nodes
+// export const RouteItems = ({routes}) => (
+//     Object.values(routes).map(({ route, label }) => (
+//      <Link to={route} label={label} key={route}>
+//      *children*
+//      </Link>
+//     ))
+// );
 
-const LinkButton = ({ to, label }) => {
+// const LinkButton = ({ to, label }) => {
+//   return (
+//     <Link to={to}>
+//       <Button>{label}</Button>
+//     </Link>
+//   );
+// };
+
+const siteName = "Księgowość Kogucik";
+
+export function Navigation({ routes }) {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <Link to={to}>
-      <Button>{label}</Button>
-    </Link>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* Narrow view */}
+          <MonetizationOnIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+          />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            {siteName}
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {Object.values(routes).map(({ route, label }) => (
+                // TODO 1 - fix styling as it's lost now
+                // TODO 2 extract route mapping to a function
+                <Link to={route} key={label}>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{label}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+
+          {/* Wide view */}
+          <MonetizationOnIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            {siteName}
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {Object.values(routes).map(({ route, label }) => (
+              // TODO extract route mapping to a function
+              <Link to={route} key={label}>
+                <Button
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                  key={label}
+                >
+                  {label}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
-};
+}
