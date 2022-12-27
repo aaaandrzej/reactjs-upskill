effe7804644a5f57822eda4a898fc90f3c19d948
Andrzej Szulc <andrzej.szulc@gmail.com>
2022-12-27 17:46:29 +0100

    Temp changes during review

--- src/Components/InvoiceForm/InvoiceForm.js
+++ src/Components/InvoiceForm/InvoiceForm.js
@@ -16,24 +16,28 @@ export default function InvoiceForm() {
   const preDefinedFields = invoiceId
     ? mockedData.filter((item) => item.id === invoiceId)[0]
     : undefined;
-  // TODO consult if calling [0] here is acceptable, and if we need if/else
 
-  const [date, setDate] = useState(
-    preDefinedFields?.date ? preDefinedFields.date : ""
-  );
+  const [date, setDate] = useState(preDefinedFields?.date || "");
 
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit, register, setValue } = useForm({
     defaultValues: {
-      // can't use preDefinedFields?.id etc due to react_devtools_backend.js:4012 Warning: A component is changing an uncontrolled input to be controlled. This is likely caused by the value changing from undefined to a defined value, which should not happen. Decide between using a controlled or uncontrolled input element for the lifetime of the component. More info: https://reactjs.org/link/controlled-components
-      // neither can use `id: preDefinedFields ? preDefinedFields.id : "",`
+      id: preDefinedFields?.id || "",
+      amount: preDefinedFields?.amount || "",
+      recipentName: preDefinedFields?.recipentName || "",
+      recipentAddress: preDefinedFields?.recipentAddress || "",
+      senderName: preDefinedFields?.senderName || "",
+      senderAddress: preDefinedFields?.senderAddress || "",
+      date: date
     },
   });
 
   // TODO convert this to saving object instead of just logging input data
   const onSubmit = (data) => {
     // TODO probably such manipulations should happen within the field and not on submit...
-    data.date = data?.date?._d ? data.date._d.toString() : data.date || "";
-    console.log(data);
+    console.log({
+      ...data,
+      date: data?.date?._d ? data.date._d.toString() : data.date || "",
+    });
   };
 
   const theme = createTheme({
@@ -46,29 +50,22 @@ export default function InvoiceForm() {
   });
 
   return (
-    <Box sx={{ margin: 1 }}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="id"
-          // can't use preDefinedFields?.id etc due to the same warning as above
-          // neither can use defaultValues at useForm level or in TextField - has to be at Controller level to work
-          defaultValue={preDefinedFields?.id ? preDefinedFields.id : ""}
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="No"
-              id="standard-basic"
-              variant="standard"
-            />
-          )}
-        />
-        <Controller
-          name="date"
-          defaultValue={date}
-          control={control}
-          render={({ field: { onChange, ...restField } }) => (
-            <LocalizationProvider dateAdapter={AdapterMoment}>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <Box
+          sx={{ margin: 1, display: "inline-flex", flexDirection: "column" }}
+        >
+          <TextField
+            {...register("id")}
+            label="No"
+            id="standard-basic"
+            variant="standard"
+          />
+          {/* <Controller
+            name="date"
+            defaultValue={date}
+            control={control}
+            render={({ field: { onChange, ...restField } }) => (
               <DatePicker
                 label="Date"
                 onChange={(event) => {
@@ -84,108 +81,79 @@ export default function InvoiceForm() {
                 )}
                 {...restField}
               />
-            </LocalizationProvider>
-          )}
-        />
-        <Controller
-          name="ammount"
-          defaultValue={preDefinedFields?.amount ? preDefinedFields.amount : ""}
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="Ammount"
-              id="standard-basic"
-              variant="standard"
-            />
-          )}
-        />
-        <Controller
-          name="recipentName"
-          defaultValue={
-            preDefinedFields?.recipentName ? preDefinedFields.recipentName : ""
-          }
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="Recipent's Name"
-              id="standard-basic"
-              variant="standard"
-            />
-          )}
-        />
-        <Controller
-          name="recipentAddress"
-          defaultValue={
-            preDefinedFields?.recipentAddress
-              ? preDefinedFields.recipentAddress
-              : ""
-          }
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="Recipent's Address"
-              id="standard-basic"
-              variant="standard"
-            />
-          )}
-        />
-        <Controller
-          name="senderName"
-          defaultValue={
-            preDefinedFields?.senderName ? preDefinedFields.senderName : ""
-          }
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="Sender's Name"
-              id="standard-basic"
-              variant="standard"
-            />
-          )}
-        />
-        <Controller
-          name="senderAddress"
-          defaultValue={
-            preDefinedFields?.senderAddress
-              ? preDefinedFields.senderAddress
-              : ""
-          }
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="Sender's Address"
-              id="standard-basic"
-              variant="standard"
-            />
-          )}
-        />
-        <div>
-          Paid
-          <Controller
-            name="isPaid"
-            // this is probably very bad but it finally works
-            defaultValue={Boolean(preDefinedFields?.isPaid)}
-            control={control}
-            render={({ field: props }) => (
-              <Checkbox
-                {...props}
-                onChange={(e) => props.onChange(Boolean(e.target.checked))}
-                defaultChecked={Boolean(preDefinedFields?.isPaid)}
-              />
+            )}
+          /> */}
+          {/* convert this to sth like: */}
+          <DatePicker
+            {...register("date")}
+            label="Date"
+            onChange={(newValue) => {
+              setValue('date', newValue);
+              setDate(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField {...params} id="standard-basic" variant="standard" />
             )}
           />
-        </div>
-        <ThemeProvider theme={theme}>
-          <Button type="submit" variant="contained" color="neutral">
-            Submit
-          </Button>
-        </ThemeProvider>
-      </form>
-    </Box>
+
+          <TextField
+            {...register("amount")}
+            label="Amount"
+            id="standard-basic"
+            variant="standard"
+          />
+
+          <TextField
+            {...register("recipentName")}
+            label="Recipent's Name"
+            id="standard-basic"
+            variant="standard"
+          />
+
+          <TextField
+            {...register("recipentAddress")}
+            label="Recipent's Address"
+            id="standard-basic"
+            variant="standard"
+          />
+
+          <TextField
+            {...register("senderName")}
+            label="Sender's Name"
+            id="standard-basic"
+            variant="standard"
+          />
+
+          <TextField
+            {...register("senderAddress")}
+            label="Sender's Address"
+            id="standard-basic"
+            variant="standard"
+          />
+
+          <div>
+            Paid
+            <Controller
+              name="isPaid"
+              // this is probably very bad but it finally works
+              defaultValue={Boolean(preDefinedFields?.isPaid)}
+              control={control}
+              render={({ field: props }) => (
+                <Checkbox
+                  {...props}
+                  onChange={(e) => props.onChange(Boolean(e.target.checked))}
+                  defaultChecked={Boolean(preDefinedFields?.isPaid)}
+                />
+              )}
+            />
+          </div>
+          <ThemeProvider theme={theme}>
+            <Button type="submit" variant="contained" color="neutral">
+              Submit
+            </Button>
+          </ThemeProvider>
+        </Box>
+      </LocalizationProvider>
+    </form>
   );
 }
--- src/Pages/EditInvoice/EditInvoice.js
+++ src/Pages/EditInvoice/EditInvoice.js
@@ -3,5 +3,6 @@ import React from "react";
 import InvoiceForm from "../../Components/InvoiceForm/InvoiceForm";
 
 export default function EditInvoice() {
+  // TODO ask backend and either provide full or empty object
   return <InvoiceForm />;
 }
