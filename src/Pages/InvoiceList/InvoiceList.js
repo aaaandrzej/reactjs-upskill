29af92b789ae3f9d00a32ff09bcabd7bb4657cfd
Andrzej Szulc <andrzej.szulc@gmail.com>
2022-12-28 17:04:35 +0100

    Get list of invoices from api

--- src/Pages/InvoiceList/InvoiceList.js
+++ src/Pages/InvoiceList/InvoiceList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -10,52 +10,72 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Link } from "react-router-dom";
 
-import invoices from "../../invoiceData.json";
+import axios from "axios";
+
+const useGetInvoices = (id = "") => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    pullInvoicesfromApi();
+  }, []);
+
+  const pullInvoicesfromApi = () => {
+    axios.get(`http://localhost:3001/invoices/${id}`).then((res) => {
+      setData(res.data);
+    });
+  };
+
+  return data;
+};
 
 export default function InvoiceList() {
-  const invoiceData = [...invoices["invoices"]];
+  const invoiceData = useGetInvoices("");
 
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>No.</TableCell>
-            <TableCell align="right">Date</TableCell>
-            <TableCell align="right">Recipent Name</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="right">Paid</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.values(invoiceData).map(
-            ({ id, date, amount, recipentName, isPaid }) => (
-              <TableRow
-                key={id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Link to={"/invoice/" + id}>{id}</Link>
-                </TableCell>
-                <TableCell align="right">{date}</TableCell>
-                <TableCell align="right">{recipentName}</TableCell>
-                <TableCell align="right">{amount}</TableCell>
-                <TableCell align="right">{isPaid ? "yes" : "no"}</TableCell>
-                <TableCell align="right">
-                  <Link to={"/invoice/" + id}>
-                    <EditIcon />
-                  </Link>
-                  <Link>
-                    {/* TODO redirect delete action properly */}
-                    <DeleteIcon />
-                  </Link>
-                </TableCell>
-              </TableRow>
-            )
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  if (invoiceData.length === 0) {
+    return <div>Not yet</div>;
+  } else {
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>No.</TableCell>
+              <TableCell align="right">Date</TableCell>
+              <TableCell align="right">Recipent Name</TableCell>
+              <TableCell align="right">Amount</TableCell>
+              <TableCell align="right">Paid</TableCell>
+              <TableCell align="right">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.values(invoiceData).map(
+              ({ id, date, amount, recipentName, isPaid }) => (
+                <TableRow
+                  key={id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    <Link to={"/invoice/" + id}>{id}</Link>
+                  </TableCell>
+                  <TableCell align="right">{date}</TableCell>
+                  <TableCell align="right">{recipentName}</TableCell>
+                  <TableCell align="right">{amount}</TableCell>
+                  <TableCell align="right">{isPaid ? "yes" : "no"}</TableCell>
+                  <TableCell align="right">
+                    <Link to={"/invoice/" + id}>
+                      <EditIcon />
+                    </Link>
+                    <Link>
+                      {/* TODO redirect delete action properly */}
+                      <DeleteIcon />
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 }
