69542f65c8fc17861dcbf6fec0bb2c6d1be90788
Andrzej Szulc <andrzej.szulc@gmail.com>
2023-01-11 19:05:22 +0100

    Fix hook mock

--- src/Components/InvoiceForm/InvoiceForm.test.js
+++ src/Components/InvoiceForm/InvoiceForm.test.js
@@ -3,37 +3,22 @@ import "@testing-library/jest-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import InvoiceForm from "./InvoiceForm";
 
-test("test1", () => {
-  
-  // TODO fix hook mock
-  let mockIsLoading = true;
+let mockIsLoading = true;
 
-  jest.mock("../../Hooks/useModifyInvoices/useModifyInvoices", () => {
-    return jest.fn(() => ({
-      isLoading: mockIsLoading,
-    }));
-  });
+jest.mock("../../Hooks/useModifyInvoices/useModifyInvoices", () => ({
+  useModifyInvoices: () => ({ isLoading: { mockIsLoading } }),
+}));
 
-  jest.mock("../../Hooks/useModifyInvoices/useModifyInvoices", () => ({
-    useModifyInvoices: jest
-      .fn()
-      .mockReturnValue({
-        data: { ...MockData },
-        isLoading: mockIsLoading,
-        error: {},
-      }),
-  }));
-
+test("progress bar is visible while loading", () => {
   render(
     <Router>
       <InvoiceForm predefinedFields={{ id: null }} />
     </Router>
   );
-
-  screen.debug();
+  const progressBar = screen.getByRole("progressbar");
+  expect(progressBar).toBeInTheDocument();
 
   // TODO tests:
-  // TODO mock isLoading to true and see if spinner is active
   // TODO click on submit with no invoice number and see if error is triggerred
   // TODO click on submit with some invoice number and see if handleSubmit was called with this data
 });
